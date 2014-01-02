7cf86744f6157e88378cf2b6a01790588e953c53
hearimm <doksul23@gmail.com>
2014-01-02 22:54:23 +0900

    Revert 22530c5..da0f68f
    This rolls back to commit 22530c5ebea20bcf0631f0d33f32adb76c6673d7.

--- GoldenSection/goldenSectionViewController.h
+++ GoldenSection/goldenSectionViewController.h
@@ -8,15 +8,12 @@
 
 #import <UIKit/UIKit.h>
 #import <iAd/iAd.h>
-//Import GADBannerView's definition from the SDK
-#import "GADBannerView.h"
 
-@interface goldenSectionViewController :UIViewController{
-    GADBannerView *bannerView_;
+@interface goldenSectionViewController : UIViewController <ADBannerViewDelegate>{
+    //ADBannerView *adview;
 }
-
-//@property (strong, nonatomic) IBOutlet ADBannerView *adView;
-//@property BOOL bannerIsVisible;
+@property (strong, nonatomic) IBOutlet ADBannerView *adView;
+@property BOOL bannerIsVisible;
 @property BOOL calcFlag;
 
 @property (strong, nonatomic) IBOutlet UITextField *highText;
--- GoldenSection/goldenSectionViewController.m
+++ GoldenSection/goldenSectionViewController.m
@@ -7,15 +7,12 @@
 //
 
 #import "goldenSectionViewController.h"
-#import "appID.h"
-
 
 @interface goldenSectionViewController ()
 
 @end
 
 @implementation goldenSectionViewController
-//@synthesize bannerView = bannerView_;
 const float GOLDEN_RATE = 1.61803398875;
 
 - (void)viewDidLoad
@@ -34,19 +31,6 @@ const float GOLDEN_RATE = 1.61803398875;
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     self.view.backgroundColor = [UIColor colorWithPatternImage:image];
-    
-    bannerView_ = [[GADBannerView alloc] initWithAdSize:kGADAdSizeBanner];
-    
-    // Specify the ad unit ID.
-    bannerView_.adUnitID = MY_BANNER_UNIT_ID;
-    
-    // Let the runtime know which UIViewController to restore after taking
-    // the user wherever the ad goes and add it to the view hierarchy.
-    bannerView_.rootViewController = self;
-    [self.view addSubview:bannerView_];
-    
-    // Initiate a generic request to load it with an ad.
-    [bannerView_ loadRequest:[GADRequest request]];
     
     _calcFlag = false;
 }
@@ -93,7 +77,7 @@ const float GOLDEN_RATE = 1.61803398875;
     _highText.text = highValue;
     //_resultText.text = resultValue;
 }
-/*
+
 - (void)bannerViewDidLoadAd:(ADBannerView *)banner
 {
     if (!self.bannerIsVisible)
@@ -116,7 +100,7 @@ const float GOLDEN_RATE = 1.61803398875;
         self.bannerIsVisible = NO;
     }
 }
-*/
+
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
 	[self.view endEditing:YES];
